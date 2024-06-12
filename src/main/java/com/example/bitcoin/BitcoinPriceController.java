0ec90ee624969bf9f1e74f899470dde22eef0681
Ajit <ajitkumar03@gmail.com>
2024-06-12 11:27:40 +0530

    removed unused lines

--- src/main/java/com/example/bitcoin/BitcoinPriceController.java
+++ src/main/java/com/example/bitcoin/BitcoinPriceController.java
@@ -7,7 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bitcoin")
